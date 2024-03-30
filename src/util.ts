45d74999116571a0fd456d553b56d517485dbab0
Adam Burdette <alburdet619@gmail.com>
2024-03-30 16:43:04 -0400

    Add searchByBookInfo function
    This function allows users to search by author and/or title as well as is allowed by the OpenLibrary API.

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,12 @@
-import { Book, Search } from './schemas.ts';
+import { Book, Search } from "./schemas.ts";
 
-const API_URL = new URL('https://openlibrary.org');
+const API_URL = new URL("https://openlibrary.org");
 
 async function _fetch(url: string | URL): Promise<unknown> {
   const result = await fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   })
     .then((response) => response.json())
@@ -19,8 +19,33 @@ async function _fetch(url: string | URL): Promise<unknown> {
 
 export async function searchBook(query: string): Promise<Search> {
   const url = API_URL;
-  url.pathname = '/search.json';
-  url.searchParams.set('q', query);
+  url.pathname = "/search.json";
+  url.searchParams.set("q", query);
+
+  const result = await _fetch(url);
+  return Search.parse(result);
+}
+
+export async function searchByBookInfo({
+  author,
+  title,
+}: {
+  author?: string;
+  title?: string;
+}): Promise<Search> {
+  const url = API_URL;
+  url.pathname = "/search.json";
+  
+  if (author) url.searchParams.set("author", author);
+  if (title) url.searchParams.set("title", title);
+  if (!author && !title) {
+    console.log("Please provide author or title to searchByBookInfo");
+    return {
+      start: 0,
+      num_found: 0,
+      docs: [],
+    };
+  }
 
   const result = await _fetch(url);
   return Search.parse(result);
